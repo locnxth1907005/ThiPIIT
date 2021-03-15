4a5f00c2896e2065f8cc84ffb42c7dbb9d80241a
locnxth1907005 <locnxth1907005@fpt.edu.vn>
2021-03-15 19:41:29 +0700

    first commit

---
+++ ThiPIIT/Models/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThiPIIT.Models
+{
+    public class Member
+
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string OTP { get; set; }
+
+        public StatusEnum Status { get; set; }
+
+    }
+    public enum StatusEnum
+    {
+        AVTIVE,DEACTIVE,DELETE
+    }
+}
